70c21f520bb37f3886573e2e6a61eab4469f07ae
Meital <meiti2002@hotmail.com>
2022-05-29 01:32:15 -0300

    primera parte

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AppCoder',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["C:/Users/Usuario/Desktop/PFinal/ProyectoFinal/AppCoder/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
